ca9dd1ced83ebf85f6b13be07a307abda1798e8c
Aakash Kumar Nain <aakashnain@outlook.com>
2015-10-30 13:10:06 +0530

    Added form with enhanced security

--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,6 @@
 from myapp import app
-from flask import render_template
+from flask import render_template,flash,redirect
+from .forms import LoginForm
 
 @app.route('/')
 def hello():
@@ -23,3 +24,13 @@ def index():
                            user = user,
                            title=title,
                            posts=posts)
+
+
+@app.route('/login', methods =['GET','POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for ID = "%s" , remember_me = %s'  %(form.openid.data,str(form.remember_me.data)))
+        return redirect('/index')
+    return render_template('login.html',title='Sign In',form=form)
+
