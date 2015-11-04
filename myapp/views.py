6e9c4e02f674e17e2a09cef398987661310ef919
Aakash Kumar Nain <aakashnain@outlook.com>
2015-11-04 16:34:29 +0530

    Login authentication done

--- myapp/views.py
+++ myapp/views.py
@@ -1,36 +1,68 @@
-from myapp import app
-from flask import render_template,flash,redirect
+from myapp import app , lm, oid
+from flask import render_template, flash, redirect, session, request, url_for, g
+from flask.ext.login import login_user , logout_user , current_user , login_required
 from .forms import LoginForm
+from .models import User,db
+
+@lm.user_loader
+def load_user(uid):
+    return User.objects.get(int(uid))
+
+@app.before_request
+def before_request():
+    g.user = current_user
 
 @app.route('/')
 def hello():
     return "Hello Nain"
 
 @app.route('/index')
+@login_required
 def index():
-    user = {'name' : 'Aakash Nain'}
-    title = "Nain's Blog"
-    posts = [
-        { 
-            'author': {'name': 'John'}, 
-            'body': 'Beautiful day in Portland!' 
-        },
-        { 
-            'author': {'name': 'Susan'}, 
-            'body': 'The Avengers movie was so cool!' 
-        }
-    ]
+    user = g.user
+    title = 'Welcome'
     return render_template('index.html',
                            user = user,
-                           title=title,
-                           posts=posts)
+                           title=title)
 
 
 @app.route('/login', methods =['GET','POST'])
+@oid.loginhandler
 def login():
+    if g.user is not None and g.user.is_authenticated:
+        return redirect(url_for('index'))
+
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for ID = "%s" , remember_me = %s'  %(form.openid.data,str(form.remember_me.data)))
-        return redirect('/index')
-    return render_template('login.html',title='Sign In',form=form)
+        session['remember_me'] = form.remember_me.data
+        return oid.try_login(form.openid.data , ask_for=['nickname','email'])
+       ## flash('Login requested for ID = "%s" , remember_me = %s'  %(form.openid.data,str(form.remember_me.data)))
+       ##return redirect('/index')
 
+    return render_template('login.html',title='Sign In',form=form , providers = app.config['OPENID_PROVIDERS'])
+
+
+@oid.after_login
+def after_login(resp):
+    if resp.email is None or resp.email=="":
+        flash("Invalid login")
+        return redirect (url_for('login'))
+
+    user = User.objects.get(email=resp.email).findOne()
+
+    if user is None:
+        nickname = resp.nickname
+        if nickname is None or nickname == "":
+            nickname = resp.email.split('@')[0]
+
+        user = User(nickname = nickname,email=resp.email)
+        db.session.add(user)
+        db.session.commit()
+    remember_me = False
+
+    if 'remember_me' in session :
+        remember_me = session['remember_me']
+        session.pop('remember_me',None)
+
+    login_user(user , remember = remember_me)
+    return redirect(request.args.get('next') or url_for('index'))
